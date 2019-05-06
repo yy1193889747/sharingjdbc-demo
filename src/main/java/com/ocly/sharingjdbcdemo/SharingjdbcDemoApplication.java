6bc52c426153432431133f6c5ec514b6d8f7d8d4
yy1193889747 <1193889747@qq.com>
2019-05-06 11:13:26 +0800

    Initial commit

---
+++ src/main/java/com/ocly/sharingjdbcdemo/SharingjdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ocly.sharingjdbcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingjdbcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingjdbcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocly/sharingjdbcdemo/SharingjdbcDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ocly.sharingjdbcdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SharingjdbcDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
